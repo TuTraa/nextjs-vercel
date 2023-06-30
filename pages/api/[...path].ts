b39661729a60ed2fe7c8e3516795a2fc368d1187
tutran <tranvantu982001@gmail.com>
2023-06-30 14:25:32 +0700

    use url

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -24,7 +24,7 @@ export default function handler(
 
     //
     proxy.web(req, res, {
-        target: process.env.API_URL,
+        target: "https://js-post-api.herokuapp.com",
         changeOrigin: true,
         selfHandleResponse: false,
     })
